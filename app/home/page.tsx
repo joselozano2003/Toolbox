6a61f599577abc27b9a66661510047c5738e4efe
JoseLozano2003 <josecamilo.lozanocet@ucalgary.ca>
2023-06-04 23:47:27 -0600

    Styles done

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function Home() {
     return(
         <div className={styles.parent}>
-            <h1>Dashboard</h1>
+            <h1 className='title'>Dashboard</h1>
             <div className={styles.cards}>
                 {tools.map((tool) => (
                     <div className=''>
